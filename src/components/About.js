e0c3940bdc5e7ec5f93f33575042251a22cd4320
Kristina Alabastro <kristinaa7@live.com>
2023-04-10 23:11:41 -0700

    added sample code for about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,18 @@
-function About() {}
+import React from "react";
+
+function About() {
+    return (
+        <section id="about">
+            <h1> Hi, I'm Kristina </h1>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
+                laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
+                Laborum, voluptas natus? </p>
+            <div>
+                <a href="#contact"> Contact Information </a>
+                <a href="#projects"> See My Past Work </a>
+            </div>
+        </section>
+    )
+}
 
 export default About;
