47e7c895e34f4315c43a5e0b4b1748f42addaa5e
Kristina Alabastro <kristinaa7@live.com>
2023-04-10 23:06:58 -0700

    added header and footer components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
 
 function App() {
-  return (
-    <main>
-      <Navbar />
-      <About />
-      <Projects />
-      <Skills />
-      <Testimonials />
-      <Contact />
-    </main>
-  );
+    return (
+        <main>
+            <Header />
+            <Navigation />
+            <About />
+            <Projects />
+            <Skills />
+            <Testimonials />
+            <Contact />
+            <Footer />
+        </main>
+    );
 }
 
 export default App;
