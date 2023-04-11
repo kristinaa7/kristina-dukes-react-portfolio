0041ffab53b108a86850dcbd968620e556afab66
Kristina Alabastro <kristinaa7@live.com>
2023-04-10 23:19:57 -0700

    added sample code to contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,13 @@
-function Contact() {}
+import React from "react";
+
+function Contact() {
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [message, setMessage] = React.useState("");
+    
+    <section id="contact">
+
+    </section>
+}
 
 export default Contact;
