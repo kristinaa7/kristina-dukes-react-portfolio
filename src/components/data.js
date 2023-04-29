ed8ffa4c62b08d80b72ff3f1531acebbbcf1e909
Kristina Alabastro <kristinaa7@live.com>
2023-04-28 23:07:50 -0700

    added projects in data file

--- src/components/data.js
+++ src/components/data.js
@@ -5,6 +5,29 @@ export const projects = [
       description:
         "A social media application for posting and viewing pictures of the sky.",
       image: "lookup.png",
-      link: "https://reactbootcamp.com",
+      link: "https://look-up-project-2.herokuapp.com/",
     },
+    {
+        title: "Cinerank",
+        subtitle: "Group Project",
+        description:
+          "A movie-goer's best friend for obtaining information on a movie.",
+        image: "cinerank.png",
+        link: "https://github.com/kristinaa7/personal-cinerank",
+      },
+      {
+        title: "Coding Quiz",
+        subtitle: "Javascript Challenge Assignment",
+        description:
+          "A fun javascript quiz for new coders.",
+        image: "codingquiz.png",
+        link: "https://github.com/kristinaa7/coding-quiz-for-beginners-only",
+      },
+      {
+        title: "Workday Scheduler",
+        subtitle: "Third-Party APIs Challenge Assignment",
+        description: "Organizing your work day with this user friendly design application.",
+        image: "workdayscheduler.png",
+        link: "https://github.com/kristinaa7/work-day-scheduler",
+      },
 ]
