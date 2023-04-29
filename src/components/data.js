02725853688c75bf4ba042d0fd9c166fab174751
Kristina Alabastro <kristinaa7@live.com>
2023-04-28 22:57:59 -0700

    added project to data.js

---
+++ src/components/data.js
@@ -0,0 +1,10 @@
+export const projects = [
+    {
+      title: "Look-Up",
+      subtitle: "Group Project",
+      description:
+        "A social media application for posting and viewing pictures of the sky.",
+      image: "lookup.png",
+      link: "https://reactbootcamp.com",
+    },
+]
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { projects } from "../data";
+
 
 function Projects() {
     return (
