6a60d6c733db62a227707603d10c1d10f57ac262
Kristina Alabastro <kristinaa7@live.com>
2023-05-01 21:27:35 -0700

    added matchmyneeds

--- src/components/data.js
+++ src/components/data.js
@@ -1,49 +1,57 @@
 export const projects = [
-    {
-      title: "Look-Up",
-      subtitle: "Group Project",
-      description:
-        "A social media application for posting and viewing pictures of the sky.",
-      image: "assets/images/lookup.png",
-      link: "https://look-up-project-2.herokuapp.com/",
-    },
-    {
-        title: "Cinerank",
-        subtitle: "Group Project",
-        description:
-          "A movie-goer's best friend for obtaining information on a movie.",
-        image: "assets/images/cinerank.png",
-        link: "https://github.com/kristinaa7/personal-cinerank",
-      },
-      {
-        title: "Coding Quiz",
-        subtitle: "Javascript Challenge Assignment",
-        description:
-          "A fun javascript quiz for new coders.",
-        image: "assets/images/codingquiz.png",
-        link: "https://github.com/kristinaa7/coding-quiz-for-beginners-only",
-      },
-      {
-        title: "Workday Scheduler",
-        subtitle: "Third-Party APIs Challenge Assignment",
-        description: "Organizing your work day with this user friendly design application.",
-        image: "assets/images/workdayscheduler.png",
-        link: "https://github.com/kristinaa7/work-day-scheduler",
-      },
-      {
-        title: "Refactoring Starter Code",
-        subtitle: "First Challenge to Debug Starter Code Assignment",
-        description:
-          "Refactoring a website to simply make it more accessible and functional for users using CSS and HTML",
-        image: "assets/images/debugging.png",
-        link: "https://github.com/kristinaa7/intro-to-cleaning-up-starter-code",
-      },
-      {
-        title: "Password Generator",
-        subtitle: "Javascript Challenge Assignment",
-        description:
-          "User-friendly random password generator that will create passwords ranging from 8-128 characters",
-        image: "assets/images/passwordgenerator.png",
-        link: "https://github.com/kristinaa7/unique-random-password-generator",
-      },
+  {
+    title: "MatchMyNeeds",
+    subtitle: "MERN GraphQL Group Project",
+    description:
+      "A hub for  for requesting items.",
+    image: "assets/images/matchmyneeds.png",
+    link: "https://matchmyneeds.herokuapp.com/",
+  },
+  {
+    title: "Look-Up",
+    subtitle: "Group Project",
+    description:
+      "A social media application for posting and viewing pictures of the sky.",
+    image: "assets/images/lookup.png",
+    link: "https://look-up-project-2.herokuapp.com/",
+  },
+  {
+    title: "Cinerank",
+    subtitle: "Group Project",
+    description:
+      "A movie-goer's best friend for obtaining information on a movie.",
+    image: "assets/images/cinerank.png",
+    link: "https://github.com/kristinaa7/personal-cinerank",
+  },
+  {
+    title: "Coding Quiz",
+    subtitle: "Javascript Challenge Assignment",
+    description:
+      "A fun javascript quiz for new coders.",
+    image: "assets/images/codingquiz.png",
+    link: "https://github.com/kristinaa7/coding-quiz-for-beginners-only",
+  },
+  {
+    title: "Workday Scheduler",
+    subtitle: "Third-Party APIs Challenge Assignment",
+    description: "Organizing your work day with this user friendly design application.",
+    image: "assets/images/workdayscheduler.png",
+    link: "https://github.com/kristinaa7/work-day-scheduler",
+  },
+  {
+    title: "Refactoring Starter Code",
+    subtitle: "First Challenge to Debug Starter Code Assignment",
+    description:
+      "Refactoring a website to simply make it more accessible and functional for users using CSS and HTML",
+    image: "assets/images/debugging.png",
+    link: "https://github.com/kristinaa7/intro-to-cleaning-up-starter-code",
+  },
+  {
+    title: "Password Generator",
+    subtitle: "Javascript Challenge Assignment",
+    description:
+      "User-friendly random password generator that will create passwords ranging from 8-128 characters",
+    image: "assets/images/passwordgenerator.png",
+    link: "https://github.com/kristinaa7/unique-random-password-generator",
+  },
 ]
