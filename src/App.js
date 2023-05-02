1b6793ff4bd095f61617392d9532eda5c7f47910
Kristina Alabastro <kristinaa7@live.com>
2023-05-01 22:02:00 -0700

    worked on deployment

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from "react";
 import '../src/App.css';
-import PortfolioContainer from "./components/PortfolioContainer";
+// import PortfolioContainer from "./components/PortfolioContainer";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Portfolio from './components/pages/Portfolio';
+import Contact from './components/pages/Contact';
+import Resume from './components/pages/Resume';
 
 const App = () => {
     return (
-        <div>
-    <Header/>
-     <PortfolioContainer />
-     <Footer/> 
-     </div>
-     )
-     };
+        <Router>
+            <div>
+                <Header />
+                <Navigation/>
+                <Routes>
+                    <Route path='/' component={Home} />
+                    <Route path='/about' component={About} />
+                    <Route path='/portfolio' component={Portfolio} />
+                    <Route path='/resume' component={Resume} />
+                    <Route path='/contact' component={Contact} />
+                </Routes>
+                <Footer />
+            </div>
+        </Router>
+    )
+};
 
 export default App;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,61 +1,59 @@
 import React from 'react';
+import {Link, useLocation} from 'react-router-dom';
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
-// We assign them to their own variable names
-function NavTabs({ currentPage, handlePageChange }) {
+// We assign them to their own variable 
+function NavTabs() {
+  let Location = useLocation
+  console.log(Location.pathName)
   return (
     <div>
     <ul className="nav nav-tabs">
       <li className="nav-item">
-        <a
-          href="#home"
-          onClick={() => handlePageChange('Home')}
+        <Link
+          to="/"
           // This is a conditional (ternary) operator that checks to see if the current page is "Home"
           // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+          className={Location === 'Home' ? 'nav-link active' : 'nav-link'}
         >
           Home
-        </a>
+        </Link>
       </li>
       <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
+      <Link
+          to="/about"
           // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+          className={Location === 'About' ? 'nav-link active' : 'nav-link'}
         >
           About
-        </a>
+          </Link>
       </li>
       <li className="nav-item">
-        <a
-          href="#portfolio"
-          onClick={() => handlePageChange('Portfolio')}
+      <Link
+          to="/portfolio"
           // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+          className={Location === 'Portfolio' ? 'nav-link active' : 'nav-link'}
         >
           Portfolio
-        </a>
+          </Link>
       </li>
       <li className="nav-item">
-        <a
-          href="#Resume"
-          onClick={() => handlePageChange('Resume')}
+      <Link
+          to="/resume"
           // Check to see if the currentPage is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+          className={Location === 'Resume' ? 'nav-link active' : 'nav-link'}
         >
           Resume
-        </a>
+          </Link>
       </li>
       <li className="nav-item">
-        <a
-          href="#contact"
-          onClick={() => handlePageChange('Contact')}
+      <Link
+          to="/contact"
           // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+          className={Location === 'Contact' ? 'nav-link active' : 'nav-link'}
         >
           Contact
-        </a>
+          </Link>
       </li>
     </ul>
     </div>
